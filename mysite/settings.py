5a900826daea8a29beff19876040b66c5f0d8045
NeelBit <nelbertran@gmail.com>
2021-05-07 22:13:41 -0300

    cambios en setting y requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,15 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
-
 # proteger keys
 import os
-from dotenv import load_dotenv
-load_dotenv()
+"""from dotenv import load_dotenv
+load_dotenv() """
 
 from decouple import config
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
