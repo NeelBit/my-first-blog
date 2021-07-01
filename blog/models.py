0b8255196408af37a62f988ebb180c8e43b40bfc
NeelBit <nelbertran@gmail.com>
2021-07-01 02:28:24 -0300

    Comentarios en blog y colores

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,24 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+    
     class Meta():
         verbose_name = "Post"
         verbose_name_plural = "Posts"
+        
+class Comment(models.Model):
+    # related_name='comments' nos permite acceder desde el model Post
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
